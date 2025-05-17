d825d05def0741c7cddf70107909c6783eb5b82c
Andrei Vologdin <youristvl@gmail.com>
2025-05-17 16:07:46 +0300

    Написал тесты на проверку названия Issue в репозитории qa-guru/knowledge-base

---
+++ src/test/java/allure/WebSteps.java
@@ -0,0 +1,40 @@
+package allure;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+    @Step("Открываем главную страницу")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Ищем репозиторий {repo}")
+    public void searchRepository(String repo) {
+        $(".search-input-container").click();
+        $("#query-builder-test").sendKeys(repo);
+        $("#query-builder-test").submit();
+    }
+
+    @Step("Кликаем по ссылке репозитория {repo}")
+    public void clickToRepository(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Проверяем, что название вкладки = {tabName}")
+    public void checkTabName(String tabName) {
+        $("[data-content=Issues]").shouldHave(text(tabName));
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
